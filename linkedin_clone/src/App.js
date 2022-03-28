6a9a5b458ce55ed4ec0d689deaef03ec526825cb
Ebuka <eyaebuka@gmail.com>
2022-03-28 11:55:30 +0200

    Built grid and initialize project

---
+++ linkedin_clone/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import MainPage from './components/Main-page';
+
+
+function App() {
+  return (
+    <>
+      <MainPage />
+
+      
+    </>
+  );
+}
+
+export default App;
---
+++ linkedin_clone/src/components/Main-page.jsx
@@ -0,0 +1,30 @@
+import "../styles/mainPage.css"
+
+
+
+const MainPage = () =>{
+
+
+
+
+    return(
+        <>
+                
+                <div className="mainContainer">
+                        <div className="wrapperFlex">
+                            <div className="left">
+                            {/* all components on the left */}
+                            
+                            </div>
+                            <div className="right">
+                                {/* all components on the left */}
+                            </div>
+                        </div>
+                </div>
+        </>
+    )
+}
+
+
+
+export default MainPage
