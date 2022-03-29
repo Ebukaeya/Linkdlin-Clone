28fb4d6a2858bce5058c365cfaba6a2fea781526
Amin Atashkhayer <amin.atashin@gmail.com>
2022-03-29 13:46:35 +0200

    login

--- linkedin_clone/src/App.js
+++ linkedin_clone/src/App.js
@@ -1,17 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import LoginPage from "./components/Login-Page";
 import MainPage from "./components/Main-page";
-import MyFooter from "./components/MyFooter";
+
 import MyNavbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-     <MyNavbar/>
-      <MainPage />
-     {/*  <MyFooter/> */}
-    </>
+    <BrowserRouter>
+      <>
+        <MyNavbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+
+        {/*  <MyFooter/> */}
+      </>
+    </BrowserRouter>
   );
 }
 
